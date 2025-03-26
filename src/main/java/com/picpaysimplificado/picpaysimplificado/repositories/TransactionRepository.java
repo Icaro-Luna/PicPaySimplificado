59819df3218c972fc602b1f9d3a139822ad85db6
Icaro-Luna <58940274+Icaro-Luna@users.noreply.github.com>
2025-03-26 20:11:27 -0300

    feat: create TransactionRepository

--- src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/TransactionRepository.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/TransactionRepository.java
@@ -1,5 +1,6 @@
 package com.picpaysimplificado.picpaysimplificado.repositories;
 
+import com.picpaysimplificado.picpaysimplificado.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository <Transaction, Long> {
